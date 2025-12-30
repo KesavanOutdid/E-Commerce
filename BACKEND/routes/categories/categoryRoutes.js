92020f527f1977f39356f1f74a9b4a56ae79fc90
Sai santhan <saisanthanyeddula@gmail.com>
2025-12-30 18:23:46 +0530

    UpdatedCaegoryauthMiddleware

--- BACKEND/routes/categories/categoryRoutes.js
+++ BACKEND/routes/categories/categoryRoutes.js
@@ -2,17 +2,18 @@ const express = require('express');
 const router = express.Router();
 const adminCategoryController = require('../../controllers/categories/admin/categoryController');
 const websiteCategoryController = require('../../controllers/categories/website/categoryController');
+const authmiddleware = require('../../middlewares/authMiddleware');
 
 // --- MAIN CATEGORY ROUTES ---
-router.post('/main', adminCategoryController.createMainCategory);
-router.put('/main/:id', adminCategoryController.updateMainCategory);
-router.delete('/main/:id', adminCategoryController.deleteMainCategory);
+router.post('/main',authmiddleware, adminCategoryController.createMainCategory);
+router.put('/main/:id',authmiddleware, adminCategoryController.updateMainCategory);
+router.delete('/main/:id',authmiddleware, adminCategoryController.deleteMainCategory);
 router.get('/main', adminCategoryController.getMainCategories);
 
 // --- SUBCATEGORY ROUTES ---
-router.post('/sub', adminCategoryController.createSubcategory);
-router.put('/sub/:id', adminCategoryController.updateSubcategory);
-router.delete('/sub/:id', adminCategoryController.deleteSubcategory);
+router.post('/sub',authmiddleware, adminCategoryController.createSubcategory);
+router.put('/sub/:id', authmiddleware, adminCategoryController.updateSubcategory);
+router.delete('/sub/:id', authmiddleware, adminCategoryController.deleteSubcategory);
 router.get('/sub/:parentId', adminCategoryController.getSubcategoriesByParent);
 router.post('/sub/:id/attributes', adminCategoryController.addSubcategoryAttributes);
 
