f1c9ebf1ea572176e0ae65f6b982b16faeb86ea9
KesavanOutdid <kesavan@outdidtech.com>
2026-01-22 14:45:51 +0530

    Updated

---
+++ FRONTEND/SELLER/.next/static/chunks/src_app_(site)_products_edit_[id]_page_tsx_d5277eab._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_(site)_products_edit_[id]_page_tsx_d5277eab._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_(site)_products_edit_[id]_page_tsx_d5277eab._.js",
+  "chunks": [
+    "static/chunks/src_08938684._.js"
+  ],
+  "source": "dynamic"
+});
